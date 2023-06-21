c159a1b046a972e54108b8e792345c956c3f1234
lukaszdolega <lukasz-dole@wp.pl>
2023-06-21 08:41:23 +0200

    Added uploading images

--- src/app/modules/transformation/transformation-add/transformation-add.component.ts
+++ src/app/modules/transformation/transformation-add/transformation-add.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TransformationAddService } from './transformation-add.service';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Transformation } from '../model/transformation';
 
 @Component({
   selector: 'app-transformation-add',
@@ -10,50 +11,78 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrls: ['./transformation-add.component.scss']
 })
 export class TransformationAddComponent {
+
   @Input() transformationForm!: FormGroup;
+
+  requiredFileTypes = "image/jpeg, image/png";
+  imageBeforeForm!: FormGroup;
+  imageAfterForm!: FormGroup;
+  imageBefore: string | null = null;
+  imageAfter: string | null = null;
+
   constructor(
     private formBuilder: FormBuilder,
     private transformationAddService: TransformationAddService,
     private router: Router,
     private snackBar: MatSnackBar
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.transformationForm = this.formBuilder.group({
       weightBefore: ['', [Validators.required, Validators.min(1)]],
       weightAfter: ['', [Validators.required, Validators.min(1)]],
-      imageBefore: ['', [Validators.required, Validators.minLength(4)]],
-      imageAfter: ['', [Validators.required, Validators.minLength(4)]],
       duration: ['', [Validators.required, Validators.min(1)]],
+    });
+    this.imageBeforeForm = this.formBuilder.group({
+      file: ['']
+    });
+    this.imageAfterForm = this.formBuilder.group({
+      file: ['']
     });
   }
 
-  submit(){
-    this.transformationAddService.saveNewTransformation(this.transformationForm.value)
-    .subscribe(transformation => {
-      this.router.navigate(["/transformations"])
-        .then(() => this.snackBar.open("Your transformation has been saved", "", {duration:3000}))
-    })
+  submit() {
+    this.transformationAddService.saveNewTransformation({
+      weightBefore: this.transformationForm.get('weightBefore')?.value,
+      weightAfter: this.transformationForm.get('weightAfter')?.value,
+      duration: this.transformationForm.get('duration')?.value,
+      imageBefore: this.imageBefore,
+      imageAfter: this.imageAfter
+    } as Transformation)
+      .subscribe(transformation => {
+        this.router.navigate(["/transformations"])
+          .then(() => this.snackBar.open("Your transformation has been saved", "", { duration: 3000 }))
+      })
   }
 
-  get weightBefore(){
-    return this.transformationForm.get("weightBefore");
+  uploadImageBefore() {
+    let formData = new FormData();
+    formData.append('file', this.imageBeforeForm.get('file')?.value);
+    this.transformationAddService.uploadImage(formData)
+      .subscribe(result => this.imageBefore = result.filename);
   }
 
-  get weightAfter(){
-    return this.transformationForm.get("weightAfter");
+  uploadImageAfter() {
+    let formData = new FormData();
+    formData.append('file', this.imageAfterForm.get('file')?.value);
+    this.transformationAddService.uploadImage(formData)
+      .subscribe(result => this.imageAfter = result.filename);
   }
 
-  get imageBefore(){
-    return this.transformationForm.get("imageBefore");
+  onImageBeforeChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.imageBeforeForm.patchValue({
+        file: event.target.files[0]
+      });
+    }
   }
 
-  get imageAfter(){
-    return this.transformationForm.get("imageAfter");
-  }
-
-  get duration(){
-    return this.transformationForm.get("duration");
+  onImageAfterChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.imageAfterForm.patchValue({
+        file: event.target.files[0]
+      });
+    }
   }
 }
 
--- src/app/modules/transformation/transformation-add/transformation-add.service.ts
+++ src/app/modules/transformation/transformation-add/transformation-add.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Transformation } from '../model/transformation';
 import { Observable } from 'rxjs';
+import { UploadResponse } from '../model/uploadResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +12,9 @@ export class TransformationAddService {
 
   saveNewTransformation(transformation: any): Observable<any>{
     return this.http.post<any>("/api/transformations", transformation);
+  }
+
+  uploadImage(formData: FormData): Observable<UploadResponse> {
+    return this.http.post<UploadResponse>('/api/transformations/upload-image', formData);
   }
 }
